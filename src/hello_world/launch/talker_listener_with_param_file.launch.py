4d1e58bfd3a4ae8a1ebd04da1ea7cd8308bdb829
Yutaka Kondo <yutaka.kondo@youtalk.jp>
2024-07-25 06:18:32 +0900

    update params_file

--- src/hello_world/launch/talker_listener_with_param_file.launch.py
+++ src/hello_world/launch/talker_listener_with_param_file.launch.py
@@ -13,21 +13,24 @@
 # limitations under the License.
 
 from launch import LaunchDescription
+import launch.actions
 import launch.substitutions
 import launch_ros.actions
+import os
 
 
 def generate_launch_description():
     params_file = launch.substitutions.LaunchConfiguration(
-        'params', default=[launch.substitutions.ThisLaunchFileDir(),
-                           '/params.yaml'])
+        'params_file')
 
     return LaunchDescription([
+        launch.actions.DeclareLaunchArgument(
+        'params_file', default_value=os.path.join(
+            os.path.dirname(__file__), 'params.yaml')),
         launch_ros.actions.Node(
             package='hello_world', namespace='hello_world',
             executable='talker_with_service_param',
-            name='talker', output='screen',
-            parameters=[params_file]),
+            name='talker', output='screen'),
         launch_ros.actions.Node(
             package='hello_world', namespace='hello_world',
             executable='listener', name='listener',
