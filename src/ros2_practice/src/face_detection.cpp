6de21232d7ff54084af4855b461c038653b53382
Yutaka Kondo <yutaka.kondo@youtalk.jp>
2024-08-03 06:46:38 +0900

    fix hello_world

--- src/ros2_practice/src/face_detection.cpp
+++ src/ros2_practice/src/face_detection.cpp
@@ -16,8 +16,8 @@
 #include <vector>
 
 #include <rclcpp/rclcpp.hpp>
-#include <cv_bridge/cv_bridge.h>
-#include <image_transport/image_transport.h>
+#include <cv_bridge/cv_bridge.hpp>
+#include <image_transport/image_transport.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
 #include <opencv2/highgui/highgui.hpp>
 #include <sensor_msgs/image_encodings.hpp>
