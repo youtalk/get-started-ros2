14874303fd8025029c0b023f819b80943338ae66
Yutaka Kondo <yutaka.kondo@youtalk.jp>
2024-08-29 23:30:37 +0900

    rm config

--- src/open_manipulator_x_moveit_config/launch/moveit_rviz.launch.py
+++
@@ -1,7 +0,0 @@
-from moveit_configs_utils import MoveItConfigsBuilder
-from moveit_configs_utils.launches import generate_moveit_rviz_launch
-
-
-def generate_launch_description():
-    moveit_config = MoveItConfigsBuilder("open_manipulator_x", package_name="open_manipulator_x_moveit_config").to_moveit_configs()
-    return generate_moveit_rviz_launch(moveit_config)
